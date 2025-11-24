40650d8e9053fa4460fb5b938cfdd4ea695e3c7c
Luis <luis@MacBook-Pro-de-Luis.local>
2025-11-24 11:23:37 -0300

    quita auth_check

--- api/guardar_servicio.php
+++ api/guardar_servicio.php
@@ -2,7 +2,6 @@
 // api/guardar_servicio.php
 header('Content-Type: application/json');
 require_once __DIR__ . '/../config.php';
-require_once __DIR__ . '/../includes/auth_check.php';
 
 try {
     $data = json_decode(file_get_contents('php://input'), true);
