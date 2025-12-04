c314887249329ec13b7f424cf9eb6046db7640d6
Luis <luis@MacBook-Pro-de-Luis.local>
2025-12-04 14:22:00 -0300

    ajusta anchos en %

--- api/pdf_servicio.php
+++ api/pdf_servicio.php
@@ -383,13 +383,13 @@
         $html .= '<table border="0" cellpadding="2" cellspacing="0" style="width: 100%; border-collapse: collapse; font-size: 9pt;">'; // Tamaño de fuente base un 10% menor
             // Anchos ajustados para encabezados: MONEDA (-20%), AFECTO (-20%), IVA (-40%), TOTAL (nueva)
         $html .= '<thead><tr style="background-color: #f2f2f2;">
-            <th style="border: 1px solid #ddd; text-align: center; width: 15%;">TIPO</th>
-            <th style="border: 1px solid #ddd; text-align: center; width: 25%;">GASTOS</th>
-            <th style="border: 1px solid #ddd; text-align: center; width: 12%;">MONEDA</th> <!-- -20% -->
-            <th style="border: 1px solid #ddd; text-align: right; width: 12%;">MONTO</th>    <!-- Ancho fijo para alinear contenido -->
-            <th style="border: 1px solid #ddd; text-align: center; width: 8%;">AFECTO</th>  <!-- -20% -->
-            <th style="border: 1px solid #ddd; text-align: right; width: 6%;">IVA%</th>    <!-- -40% -->
-            <th style="border: 1px solid #ddd; text-align: right; width: 12%;">TOTAL</th>   <!-- Ancho para nueva columna -->
+            <th style="border: 1px solid #ddd; text-align: center; width: 9%;">TIPO</th>
+            <th style="border: 1px solid #ddd; text-align: center; width: 15%;">GASTOS</th>
+            <th style="border: 1px solid #ddd; text-align: center; width: 9%;">MONEDA</th> <!-- -20% -->
+            <th style="border: 1px solid #ddd; text-align: right; width: 20%;">MONTO</th>    <!-- Ancho fijo para alinear contenido -->
+            <th style="border: 1px solid #ddd; text-align: center; width: 12%;">AFECTO</th>  <!-- -20% -->
+            <th style="border: 1px solid #ddd; text-align: right; width: 10%;">IVA%</th>    <!-- -40% -->
+            <th style="border: 1px solid #ddd; text-align: right; width: 15%;">TOTAL</th>   <!-- Ancho para nueva columna -->
         </tr></thead>';
         $html .= '<tbody>';
         foreach ($gastos_datos as $g) {
@@ -401,14 +401,14 @@
             $subtotal = $esAfecto ? $monto * (1 + $iva / 100) : $monto;
             // --- Fin cálculo ---
             $html .= '<tr>';
-                $html .= '<td style="border: 1px solid #ddd;">' . $g['tipo'] . '</td>';
-                $html .= '<td style="border: 1px solid #ddd;">' . $g['gasto'] . '</td>';
-                $html .= '<td style="border: 1px solid #ddd; text-align: center;">' . $g['moneda'] . '</td>';
-                $html .= '<td style="border: 1px solid #ddd; text-align: right;">' . number_format($g['monto'], 2) . '</td>';
-                $html .= '<td style="border: 1px solid #ddd; text-align: center;">' . $g['afecto'] . '</td>';
-                $html .= '<td style="border: 1px solid #ddd; text-align: right;">' . number_format($g['iva'], 2) . '%</td>';
+                $html .= '<td style="border: 1px solid #ddd; width: 9%;">' . $g['tipo'] . '</td>';
+                $html .= '<td style="border: 1px solid #ddd; width: 15%;">' . $g['gasto'] . '</td>';
+                $html .= '<td style="border: 1px solid #ddd; text-align: center; width: 9%;">' . $g['moneda'] . '</td>';
+                $html .= '<td style="border: 1px solid #ddd; text-align: right; width: 20%;">' . number_format($g['monto'], 2) . '</td>';
+                $html .= '<td style="border: 1px solid #ddd; text-align: center; width: 12%;">' . $g['afecto'] . '</td>';
+                $html .= '<td style="border: 1px solid #ddd; text-align: right; width: 10%;">' . number_format($g['iva'], 2) . '%</td>';
                 // Nueva celda para el TOTAL calculado
-                $html .= '<td style="border: 1px solid #ddd; text-align: right;">' . number_format($subtotal, 2) . '</td>';
+                $html .= '<td style="border: 1px solid #ddd; text-align: right; width: 15%;">' . number_format($subtotal, 2) . '</td>';
             $html .= '</tr>';
         }
         $html .= '</tbody>';
