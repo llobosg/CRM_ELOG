f777fbf84f5ac03a28e48f1231778ce04403279a
Luis <luis@MacBook-Pro-de-Luis.local>
2025-12-19 21:39:27 -0300

    corrige config.php en cabecera _logic

--- pages/ro_transp_nac_logic.php
+++ pages/ro_transp_nac_logic.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../config/db.php';
+require_once __DIR__ . '/../config.php';
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, DELETE');
