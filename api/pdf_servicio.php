70cb52ae15a6a49b573fd1c836b88d68d3abb32a
Luis <luis@MacBook-Pro-de-Luis.local>
2026-01-23 15:44:19 -0300

    Ajusta PDF 8

--- api/pdf_servicio.php
+++ api/pdf_servicio.php
@@ -340,7 +340,7 @@ foreach ($costos_datos as $c) {
 
 $html .= '</tbody>';
 $html .= '<tfoot>';
-$html .= '<tr style="font-weight: bold;"><td style="border: 1px solid #ddd; text-align: right;" colspan="3">TOTALES:</td><td style="border: 1px solid #ddd; text-align: right;">' . number_format($total_costos, 2) . '</td><td style="border: 1px solid #ddd; text-align: right;">' . number_format($total_venta, 2) . '</td><td style="border: 1px solid #ddd;"></td></tr>';
+$html .= '<tr style="font-weight: bold;"><td style="border: 1px solid #ddd; text-align: right;" colspan="2">TOTALES:</td><td style="border: 1px solid #ddd; text-align: right;">' . number_format($total_costos, 2) . '</td><td style="border: 1px solid #ddd; text-align: right;">' . number_format($total_venta, 2) . '</td><td style="border: 1px solid #ddd;"></td></tr>';
 // FILAS ELIMINADAS SEGÚN REQUERIMIENTO
 /*
 <tr style="font-weight: bold;"><td style="border: 1px solid #ddd; text-align: right;" colspan="3">TOTAL PROFIT:</td><td style="border: 1px solid #ddd; text-align: right;">' . number_format($total_venta - $total_costos, 2) . '</td><td style="border: 1px solid #ddd;"></td></tr>
