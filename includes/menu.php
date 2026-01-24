9a9b3f08614dd0f72ca3236cefb3cc4a1e55d665
Luis <luis@MacBook-Pro-de-Luis.local>
2026-01-24 19:01:44 -0300

    login con roly género3

--- includes/menu.php
+++ includes/menu.php
@@ -70,11 +70,11 @@ $clase_rol = 'role-' . $rol_usuario;
             </div>
         </li>
         <!-- Badge de usuario -->
-        <div class="user-badge <?php echo $clase_rol; ?>">
-            <div class="user-avatar"><?php echo $avatar_icon; ?></div>
+        <div class="user-badge" style="text-align: right; <?php echo $clase_rol; ?>">
+            <div class="user-avatar" style="text-align: right;"><?php echo $avatar_icon; ?></div>
             <div>
-                <div><?php echo htmlspecialchars($nombre_usuario); ?></div>
-                <div style="font-size: 12px; opacity: 0.9; font-weight: 400;"><?php echo $rol_amigable; ?></div>
+                <div style="text-align: right;"><?php echo htmlspecialchars($nombre_usuario); ?></div>
+                <div style="text-align: right; font-size: 12px; opacity: 0.9; font-weight: 400;"><?php echo $rol_amigable; ?></div>
             </div>
         </div>
         <?php endif; ?>
