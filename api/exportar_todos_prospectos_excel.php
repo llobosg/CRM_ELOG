016c0b36016fb55fefd5b5412033426b0737d128
llobosg <239452687+llobosg@users.noreply.github.com>
2026-06-17 12:24:35 -0400

    arregla export prospectos

--- api/exportar_todos_prospectos_excel.php
+++ api/exportar_todos_prospectos_excel.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__ . '/../config.php';
+require_once __DIR__ . '/../includes/auth_check.php';
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
